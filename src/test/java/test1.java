9af10fd116b7ebb5757831505736914a95eefccf
shivanks <shivanks@lambdatest.com>
2025-10-06 18:39:47 +0530

    Update test1.java

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,46 +1,29 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-import java.util.List;
-
 public class test1 {
-
     WebDriver driver;
-    WebDriverWait wait;
-
-    @BeforeMethod
-    public void setUp() {
-        // Setup Chrome driver with options
+    @BeforeTest
+    public void setup() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // Increased wait for dynamic content
     }
-
     @Test
-    public void searchAndClickLambdaTest() throws InterruptedException {
-        driver.get("https://www.lambdatest.com/");
-        WebElement resource = driver.findElement(By.xpath("//*[@id=\"header\"]/nav/div/div/div[2]/div/div/div[1]/div[3]/button"));
-        resource.click();
-        WebElement videos = driver.findElement(By.xpath("//*[@id=\"header\"]/nav/div/div/div[2]/div/div/div[1]/div[3]/div/div/div/div[1]/div/div[1]/ul/li[4]/a/div[2]/p[1]"));
-        videos.click();
-
-        Thread.sleep(3000);
-
-
+    public void test1() {
+        driver.get("https://github.com/devanshsingh15");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("devanshsingh15"));
     }
-
-    @AfterMethod
+    @AfterTest
     public void tearDown() {
         if (driver != null) {
             driver.quit();
